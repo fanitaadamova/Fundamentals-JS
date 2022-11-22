58309ede27a80f605c57fbe37d1562413d299b3a
fanitaadamova <113979211+fanitaadamova@users.noreply.github.com>
2022-11-22 21:20:29 +0200

    Update 2.3.plantDiscovery.js

--- Final exam preparation and exam/2.3.plantDiscovery.js
+++ Final exam preparation and exam/2.3.plantDiscovery.js
@@ -11,47 +11,92 @@ function plantDiscovery(array) {
 
         objectOfPlants[currentPlant] = [rarity];
     }
-// objectOfPlants - двойки key и value масив
+    // objectOfPlants - двойки key и value масив
     let currentString = array.shift();
 
     while (currentString !== 'Exhibition') {
 
         let lineArr = currentString.split(': ');
         let command = lineArr[0];
-        let currentPlant = lineArr[1];
+        let currentPlantInfo = lineArr[1];
+        //stignah do tik
 
+        if (command === 'Rate') {
+            let arr = currentPlantInfo.split(' - ')
+            let plant = arr[0];
+            let rating = arr[1];
 
-         if (command === 'Rate') {
-             
-         }
+            if (objectOfPlants.hasOwnProperty(plant)) {
 
-         if (command === 'Update') {
-             
+                let currentValueArr = objectOfPlants[plant]
+                currentValueArr.push(rating);
+                objectOfPlants[plant] = currentValueArr;
+
+            } else {
+                console.log(`error`);
+            }
+        }
+
+        if (command === 'Update') {
+
+            let arr = currentPlantInfo.split(' - ')
+            let plant = arr[0];
+            let newRarity = arr[1];
+
+            if (objectOfPlants.hasOwnProperty(plant)) {
+
+                let currentValueArr = objectOfPlants[plant]
+                //така заменям нулевия елемент на масива
+                currentValueArr.shift();
+                currentValueArr.unshift(newRarity)
+                objectOfPlants[plant] = currentValueArr;
+
+            } else {
+                console.log(`error`);
+            }
         }
 
         if (command === 'Reset') {
-             
+            let plant = currentPlantInfo;
+
+            if (objectOfPlants.hasOwnProperty(plant)) {
+                let currentValueArr = objectOfPlants[plant]
+                currentValueArr.splice(1);
+
+                objectOfPlants[plant] = currentValueArr;
+            } else {
+                console.log(`error`);
+            }
         }
-
-        console.log(command);
-        console.log(currentPlant);
-        
-
 
         currentString = array.shift();
     }
 
-    console.log(objectOfPlants);
+    console.log(`Plants for the exhibition:`);
+
+    for (const plant in objectOfPlants) {
+        let arrOfPlantsInfo = objectOfPlants[plant];
+        let rarity = arrOfPlantsInfo.shift();
+        let sumOfRating = 0;
+        let avgRating = 0;
+
+        let length = arrOfPlantsInfo.length;
+
+        if (length > 0) {
+            for (let element of arrOfPlantsInfo) {
+                sumOfRating += Number(element);
+            }
+
+            avgRating = sumOfRating / length
+        }
+
+        console.log(`- ${plant}; Rarity: ${rarity}; Rating: ${avgRating.toFixed(2)}`);
+    }
+
 }
-plantDiscovery((["3",
-    "Arnoldii<->4",
-    "Woodii<->7",
-    "Welwitschia<->2",
-    "Rate: Woodii - 10",
-    "Rate: Welwitschia - 7",
-    "Rate: Arnoldii - 3",
-    "Rate: Woodii - 5",
-    "Update: Woodii - 5",
-    "Reset: Arnoldii",
-    "Exhibition"])
-)
+plantDiscovery(["2",
+"Candelabra<->10",
+"Oahu<->10",
+"Rate: Oahu - 7",
+"Rate: Candelabra - 6",
+"Exhibition"])
